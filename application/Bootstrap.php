34f263800a23dcfbf3c87913835471fed77d0f1c
foolishbird <61183@qq.com>
2016-08-31 16:28:51 +0800

    Bootstrap 的增加.
    在app->run 之前调用.可以自己初始化一些配置信息等.以_init开头的函数都会被自动加载

---
+++ application/Bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 所有在Bootstrap类中, 以_init开头的方法, 都会被Yaf调用,
+ * 这些方法, 都接受一个参数:Yaf_Dispatcher $dispatcher
+ * 调用的次序, 和申明的次序相同
+ */
+class Bootstrap extends Yaf_Bootstrap_Abstract
+{
+
+    /**
+     * 初始化配置文件
+     */
+    public function _initConfig()
+    {
+        $config = Yaf_application::app()->getConfig();
+        Yaf_Registry::set("config", $config);
+    }
+}
--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -2,6 +2,8 @@
 class IndexController extends Yaf_Controller_Abstract {
    public function indexAction() {//默认Action
        $this->getView()->assign("content", "Hello World");
+       $conf = Yaf_Registry::get('config')->get('resources.database.params')->toArray();
+       print_r($conf);
    }
 }
 ?>
